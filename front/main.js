89a3e830d23238fc79b5018a5738e66724337e41
chuckythecat <61181095+chuckythecat@users.noreply.github.com>
2023-02-04 20:38:59 +0800

    test with file upload and camera stream

---
+++ front/main.js
@@ -0,0 +1,26 @@
+async function uploadFile() {
+    let formData = new FormData();           
+    formData.append("file", fileupload.files[0]);
+    resp = await fetch(`${window.location.origin}/upload`, {
+      method: "POST", 
+      body: formData
+    });    
+}
+
+$(document).ready(() => {
+    let video = Cookies.get("p1video");
+    if(video != undefined) {
+        $("#p1camera").html(`<img src="${video}" style="width: 640px; height: 360px;" width="640" vspace="0" hspace="0" height="360">`);
+    }
+});
+
+$("#spoiler").click(function(){
+    $("#settings").slideToggle("slow");
+});
+
+$("#submit").click(() => {uploadFile();});
+
+$("#apply").click(() => {
+    Cookies.set("p1video", $("#p1video").val());
+    $("#p1camera").html(`<img src="${$("#p1video").val()}" style="width: 640px; height: 360px;" width="640" vspace="0" hspace="0" height="360">`);
+});
